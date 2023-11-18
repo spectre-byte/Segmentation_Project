f912181c6bc58f0b1e5ec90aabd77d9abd1a80f0
spectre-byte <anantsingh.abhinav@gmail.com>
2023-11-18 23:30:51 +0530

    Logging and Exception

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import find_packages,setuptools
+from setuptools import find_packages,setup
 from typing import List
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.place("\n","")for req in requirements]
+        requirements= [req.replace("\n","") for req in requirements]
 
         if  HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
+
 setup(
     name = 'Segmentation_Project',
-    version = 0.0.1,
-    author = 'Anant'
+    version = '0.0.1',
+    author = 'spectre-byte',
     author_email = 'anantsingh.abhinav@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
